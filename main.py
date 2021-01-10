8811517ae543553c71fbb88a830672ceecbe7b5c
rgoshen <rick.goshen@gmail.com>
2021-01-10 14:48:30 -0700

    install flask_sqlalchemy

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
 from flask import Flask, render_template, request, redirect, url_for
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 
-all_books = []
+# CREATE DATABASE
+
 
 
 @app.route('/')
